d37c0d8f5ba5a6b7aa5f46dfeb629f41dcaebf0d
xiaozhongcheng <xiaozhch5@mail3.sysu.edu.cn>
2021-03-31 19:57:07 +0800

    添加依赖库以及基础库

--- src/main/java/com/zh/ch/bigdata/rpm/common/ConfigFileAnalysisUtil.java
+++ src/main/java/com/zh/ch/bigdata/rpm/common/ConfigFileAnalysisUtil.java
@@ -1,5 +1,7 @@
 package com.zh.ch.bigdata.rpm.common;
 
+import com.zh.ch.bigdata.rpm.constant.Config;
+
 /**
  * @author xzc
  * @description 描述文件解析类
@@ -7,7 +9,9 @@ package com.zh.ch.bigdata.rpm.common;
  */
 public class ConfigFileAnalysisUtil {
 
-
+    public Config getConfig(String ConfigFilePath) {
+        return null;
+    }
 
 
 
